2e746cade8be604d20eb95c40b881c809b3cc4ee
Initech <inimfonebong001@gmail.com>
2024-02-15 09:30:22 +0000

    Add status code

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python3
 """ session authentication routes """
-from api.v1.views import app_views
 from flask import jsonify, request
 from models.user import User
 import os
+from api.v1.views import app_views
 
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def login():
     """ auth session """
     email = request.form.get('email', None)
-    if not email or email == "":
-        return jsonify({"error": "email missing"})
+    if not email or len(email.strip())  == 0:
+        return jsonify({"error": "email missing"}) 400
     pwd = request.form.get('password', "")
-    if pwd == "":
-        return jsonify({"error": "password missing"})
+    if pwd == "" or len(pwd.strip()) == 0:
+        return jsonify({"error": "password missing"}), 400
     try:
         users = User.search({'email': email})
-    except KeyError:
-        return jsonify({"error": "no user found for this email"})
+    except Exception:
+        return jsonify({"error": "no user found for this email"}) 404
     if len(users) > 0:
         user = users[0]
     else:
