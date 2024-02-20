f0cb74db56144a3f6f22d1fd8646d71e706dbc22
Initech <inimfonebong001@gmail.com>
2024-02-20 07:06:33 +0000

    remove args

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -39,7 +39,7 @@ class DB:
         self._session.commit()
         return user
 
-    def find_user_by(self, *args: tuple, **kwargs: dict) -> User:
+    def find_user_by(self, **kwargs: dict) -> User:
         """ retrieve a user """
 
         for key, value in kwargs.items():
