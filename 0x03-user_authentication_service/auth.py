d9aedc3098d4eb1c9b9748dcc38fa97670768ff2
Initech <inimfonebong001@gmail.com>
2024-02-20 08:05:50 +0000

    Add task 16 tto 19

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -30,7 +30,7 @@ class Auth:
         try:
             user = self._db.find_user_by(email=email)
             if user:
-                raise ValueError("User <user's email> already exists.")
+                raise ValueError(f"User {email} already exists.")
         except (NoResultFound):
             user = self._db.add_user(email, _hash_password(password))
             return user
@@ -73,3 +73,22 @@ class Auth:
         except (InvalidRequestError, NoResultFound):
             return None
         return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """ get reset password token """
+        try:
+            user = self._db.find_user_by(email=email)
+            token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=token)
+            return token
+        except NoResultFound:
+            raise ValueError
+
+    def update_password(self, reset_token: str, pwd: str) -> None:
+        """ update password """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            self._db.update_user(user.id, hashed_password=_hash_password(pwd))
+            user.reset_token = None
+        except NoResultFound:
+            raise ValueError
